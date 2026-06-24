1eee125f1b026fb261a6ba4d09cb2ce62a5117be
Nada Hamdy <nadaahasabbalh@gmail.com>
2026-06-24 20:16:23 +0200

    part1

--- ecommerce.api/Program.cs
+++ ecommerce.api/Program.cs
@@ -1,8 +1,11 @@
+using ecommerce.infastructure;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.addinfastrucrureservice(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ ecommerce.infastructure/infastructureservicesregestrition.cs
@@ -0,0 +1,20 @@
+﻿using ecommerce.infastructure.data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace ecommerce.infastructure
+{
+    public static class infastructureservicesregestrition
+    {
+        public static IServiceCollection addinfastrucrureservice(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<storeDbcontext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefultConnection"));
+            });
+            return services;
+        }
+    }
+}
